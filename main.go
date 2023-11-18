5451a07e760eaf4f7185aa732afdde3908c48d43
jpjpjp2020 <jrgnpst@gmail.com>
2023-11-18 21:45:29 +0200

    manual helper func output setup

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
+	"time"
 )
 
 // JSON incoming mock
@@ -14,29 +13,55 @@ type DashboardData struct {
 	EpExercises     []string `json:"ep_exercises"`
 }
 
+// testing main for manual utility func testing with mock JSON data:
+
 func main() {
-	http.HandleFunc("/setup-session", handleSessionSetup) // Setting up the route
-
-	fmt.Println("Server is running on http://localhost:8080")
-	http.ListenAndServe(":8080", nil) // Starting the server
-}
-
-// receive and unmarshal JSON data
-
-func handleSessionSetup(w http.ResponseWriter, r *http.Request) {
-	var data DashboardData
-
-	// Decode the JSON body into the struct
-	err := json.NewDecoder(r.Body).Decode(&data)
-	if err != nil {
-		// Handle the error
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
+	// Mock input
+	mockData := DashboardData{
+		SessionDuration: 30,
+		StartDelay:      5,
+		Mode:            "Emergency",
+		EpExercises:     []string{"brake", "swerve_left", "swerve_right"},
 	}
 
-	// SET UP additional validation or processing...
-	// ...
+	// session start
+	startTime := time.Now()
 
-	// Respond to the client
-	fmt.Fprintf(w, "Session setup received: %+v", data)
+	// Call your helper function with mock data
+	schedule := generateSchedule(mockData, startTime)
+
+	// Manual output test
+	fmt.Println("Generated Schedule:", schedule)
+
 }
+
+// data traffic example
+
+// func main() {
+// 	http.HandleFunc("/setup-session", handleSessionSetup) // Setting up the route
+
+// 	fmt.Println("Server is running on http://localhost:8080")
+// 	http.ListenAndServe(":8080", nil) // Starting the server
+// }
+
+// // receive and unmarshal JSON data
+
+// func handleSessionSetup(w http.ResponseWriter, r *http.Request) {
+// 	var data DashboardData
+
+// 	// Decode the JSON body into the struct
+// 	err := json.NewDecoder(r.Body).Decode(&data)
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusBadRequest)
+// 		return
+// 	}
+
+// 	// Assume the session starts now (or get the start time from the request)
+// 	startTime := time.Now()
+
+// 	// Generate the schedule based on the user's choices
+// 	schedule := generateSchedule(data, startTime)
+
+// 	// Respond with the schedule
+// 	json.NewEncoder(w).Encode(schedule)
+// }
--- stack.go
+++ stack.go
@@ -1,5 +1,31 @@
 package main
 
+import "time"
+
+type AudioCue struct {
+	FileName string    `json:"file_name"`
+	PlayAt   time.Time `json:"play_at"`
+}
+
 func generate_sessions_stack() {
 
+}
+
+func generateSchedule(data DashboardData, startTime time.Time) []AudioCue {
+	var schedule []AudioCue
+
+	// Calculate the initial start time with the delay
+	currentTime := startTime.Add(time.Minute * time.Duration(data.StartDelay))
+
+	for _, exercise := range data.EpExercises {
+		// Example: Add a fixed interval between exercises
+		currentTime = currentTime.Add(time.Minute * 5)
+
+		schedule = append(schedule, AudioCue{
+			FileName: exercise + ".mp3",
+			PlayAt:   currentTime,
+		})
+	}
+
+	return schedule
 }
